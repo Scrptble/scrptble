3b82d096bfc7d44ad8e1cb5755977e8f2ac076dc
Scrptble <nafay@scrptble.com>
2025-05-01 12:14:36 +0500

    Update blogPosts.jsx

--- src/data/blogPosts.jsx
+++ src/data/blogPosts.jsx
@@ -1,1 +1,6 @@
-
+// Placeholder blog post data
+const blogPosts = [
+  { id: 1, title: 'Introducing Scrptble', summary: 'Learn about our mission and vision.' },
+  { id: 2, title: 'Automate with Scrptble', summary: 'See how easy scripting can be.' },
+];
+export default blogPosts;
