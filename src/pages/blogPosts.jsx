e0c182b3c5b37ea8a8ef89ba30c55e89bc5a0072
Scrptble <nafay@scrptble.com>
2025-05-01 12:15:34 +0500

    Delete src/pages/blogPosts.jsx

--- src/pages/blogPosts.jsx
+++
@@ -1,6 +0,0 @@
-// Placeholder blog post data
-const blogPosts = [
-  { id: 1, title: 'Introducing Scrptble', summary: 'Learn about our mission and vision.' },
-  { id: 2, title: 'Automate with Scrptble', summary: 'See how easy scripting can be.' },
-];
-export default blogPosts;
